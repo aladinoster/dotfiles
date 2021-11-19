4f265d78e44db786b5bdfe0e361225a0edd1a322
Andres Ladino <aladinoster@gmail.com>
2021-11-19 11:32:56 +0100

    Add brew config from zero

---
+++ brew.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# Terminal settings
+brew install zsh
+brew install zsh-autosuggestions
+brew install zsh-git-prompt
+brew install zsh-syntax-highlighting
+brew install tree
+brew install htop
+
+# Version control 
+brew install git
+brew install gh 
+
+# Extra tools 
+brew install ffmpeg
+brew install mactex
+brew install hugo 
+brew install docker 
+brew install docker-compose
+
+# Media tools 
+brew cask install spotify
+
+# Casks 
+brew install --cask julia
+brew install --cask visual-studio-code
+brew install --cask font-source-code-pro
